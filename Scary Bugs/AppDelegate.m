147533d83fcc98d3023197399291e02d6fa9134e
Sean Behan <inbox@seanbehan.com>
2013-04-14 16:16:50 -0400

    Initial Commit

---
+++ Scary Bugs/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Scary Bugs
+//
+//  Created by Sean Behan on 4/14/13.
+//  Copyright (c) 2013 Sean Behan. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Scary Bugs/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Scary Bugs
+//
+//  Created by Sean Behan on 4/14/13.
+//  Copyright (c) 2013 Sean Behan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
